be4a104281dbd1374155dd4e402b9f3594eea916
ashpect <ashishndiitr@gmail.com>
2024-10-27 20:59:28 +0530

    updates

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 ## Setup
 
+1. Clone the repository
+2. Run `yarn install` to install all dependencies
+3. Create a `.env` file in the root directory and add the following environment variables
+```
+PORT=5000
+OPENAI_API_KEY=your_openai_api_key
+```
+4. Run client and server  : <br>
 `yarn workspace client start` to start the client <br>
 `yarn workspace server start` to start the server
 
--- server/index.js
+++ server/index.js
@@ -1,12 +1,45 @@
 const express = require("express");
-const app = express();
+const { ChatOpenAI } = require("@langchain/openai");
+const { HumanMessage, SystemMessage } = require("@langchain/core/messages");
+const { StringOutputParser } = require("@langchain/core/output_parsers");
 
+const dotenv = require("dotenv");
+const path = require("path");
+const envPath = path.join(__dirname, '..', '.env');
+
+dotenv.config({ path: envPath });
+
+const app = express();
 const PORT = process.env.PORT || 3001;
+
+const parser = new StringOutputParser();
+const model = new ChatOpenAI({
+  model: "gpt-4",
+  openAIApiKey: process.env.OPENAI_API_KEY
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.listen(PORT, () => {
+  console.log(process.env.OPENAI_API_KEY);
+  console.log(console.log(path.resolve(__dirname, '..', '.env')))
+  console.log(`Current directory: ${__dirname}`);
+  console.log(process.env)
   console.log(`Server is running on port ${PORT}`);
+  // callGPT().catch(console.error);
 });
+
+
+ const callGPT = async () => {
+  const messages = [
+    new SystemMessage("Translate the following from English into Italian"),
+    new HumanMessage("hi!")
+  ];
+
+  const result = await model.invoke(messages);
+  const result2 = await parser.invoke(result);
+
+  console.log(result2);
+};
